ea9e85f318a1432be55efba35a286e57171d2eaf
adelinazdes <adelina.ch15@mail.com>
2023-12-09 15:11:03 +0300

    одну серу выводит

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,77 +1,86 @@
-﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include <iostream>
-#include <fstream>
-#include <string.h>
+﻿#include <string.h>
 #include <stdio.h>
+#include <clocale>
+#include <iomanip>
+#include <iostream>
 #include <vector>
+#include <fstream>
+#include <time.h>
+#include <algorithm>
+#include <fixed/fixed.h>
+#include <pde_solvers/pde_solvers.h>
 
 using namespace std;
+
+struct znachenia
+{
+    vector<double> massiv;
+};
+
+struct pipe
+{
+    double L, dx, dt, V;
+};
+
+int grid_size = 101;
+int T = 7000; //Заданный период времени 
+
 int main()
 {
-    struct pipe
-    {
-        double L, dx, dt, V;
-    };
-
     pipe myPipe;
     myPipe.L = 100000;
     myPipe.V = 1.5;
-    int n = 101;
-    int T = 7000;    //Заданный период времени 
+
     double ro_n = 850;
     double ro_k = 860;
-    myPipe.dx = myPipe.L / (n - 1);
-    myPipe.dt = myPipe.dx / myPipe.V;    //условие Куранта
+    myPipe.dx = myPipe.L / (grid_size - 1);
+    myPipe.dt = myPipe.dx / myPipe.V; //условие Куранта
     int N = (T / myPipe.dt);
 
-    vector<double> ro_0(N);
-    ro_0 = { 850, 870, 860, 860, 860,860, 860, 860, 860, 860 };
-    ofstream outFile1("massiv2.csv");
-    outFile1 << "время,координата,плотность" << "\n";
-    vector<double> ro_1(n);
-    for (int k = 0; k < n; k++) {
-        ro_1[k] = ro_n;
-    }
-    vector<double> ro_2(n);
-    for (int i = 0; i < N; i++) {
-        ro_2[0] = ro_0[i];
-        for (int i = 1; i < n; i++) {
-            ro_2[i] = ro_1[i - 1];
-        }
-        ro_1 = ro_2;
-        for (int j = 0; j < n; j++) {
-            outFile1 << i * myPipe.dt << "," << j * myPipe.dx << "," << ro_2[j] << "\n";
-        }
+    znachenia ro;
+    ro.massiv = vector<double>(N);
+    ro.massiv = { 850, 870, 860, 860, 860,860, 860, 860, 860, 860 }; //значения плотности на входе трубы
 
-    }
+    znachenia sera;
+    sera.massiv = vector<double>(N);
+    sera.massiv = { 0.01, 0.02, 0.03, 0.04, 0.05,0.06, 0.07, 0.08, 0.09, 0.01 }; //значения серы на входе трубы
 
-    outFile1.close();
-
-    //апвапимвпаив
-
-    vector<float> sera_0(N);//сера по партиям 
-    sera_0 = { 0.01, 0.02, 0.03, 0.04, 0.05,0.06, 0.07, 0.08, 0.09, 0.01 };
-    ofstream outFile2("sera2.csv");
-    outFile2 << "время,координата,сера" << "\n";
-    vector<float> sera_1(n);
-    for (int k = 0; k < n; k++) {
-        sera_1[k] = 0.01;
-    }
-    vector<float> sera_2(n);
-    for (int i = 0; i < N; i++) {
-        sera_2[0] = sera_0[i];
-        for (int i = 1; i < n; i++) {
-            sera_2[i] = sera_1[i - 1];
-        }
-        sera_1 = sera_2;
-        for (int j = 0; j < n; j++) {
-            outFile2 << myPipe.dt * i << "," << myPipe.dx * j << "," << sera_2[j] << "\n";
-        }
-    }
-    outFile2.close();
+    
 }
 
-//GHJ
+void rashet(pipe myPipe, znachenia ro, znachenia sera) {
+    vector<double> initial_layer(grid_size, ro.massiv[0]);
+    // initial_layer - слой, значениями из которого проинициализируются все слои буфера
+    ring_buffer_t<vector<double>> buffer(2, initial_layer);
+
+    for (double time = 0; time < T; time += myPipe.dt)
+    {
+        // Получение ссылок на текущий и предыдущий слои буфера
+        vector<double>& previous_layer = buffer.previous();
+        vector<double>& current_layer = buffer.current();
+
+        // Расчет методом характеристик
+        // Суть - смещение предыдущего слоя и запись граничного условия
+        for (size_t i = 1; i < current_layer.size(); i++)
+        {
+            current_layer[i] = previous_layer[i - 1];
+        }
+        current_layer[0] = ro.massiv[0];
+
+        // Слой current_layer на следующем шаге должен стать предыдущим. 
+        // Для этого сместим индекс текущего слоя в буфере на единицу
+        buffer.advance(+1);
+        
+    }
+}
+
+
+void excel(pipe myPipe, znachenia ro, znachenia sera) {
+    ofstream outFile("ddddddddddddddddd.csv");
+    outFile << "время,координата,плотность" << "\n";
+    vector<double> znachenia(grid_size);
+    for (int j = 0; j < grid_size; j++) {
+        outFile << j * myPipe.dt << "," << j * myPipe.dx << "," << znachenia[j] << "\n";
+    }
+    outFile.close();
+}
---
+++ ConsoleApplication1/замечания/ConsoleApplication1.cpp
@@ -0,0 +1,113 @@
+﻿#include <string.h>
+#include <stdio.h>
+#include <clocale>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <time.h>
+#include <algorithm>
+#include <fixed/fixed.h>
+#include <pde_solvers/pde_solvers.h>
+
+using namespace std;
+
+struct znachenia
+{
+    vector<double> massiv;
+    double nachalo;
+};
+
+struct pipe
+{
+    double L, dx, dt, V, N;
+};
+
+int grid_size = 101;
+int T = 100000; //Заданный период времени 
+
+
+
+void rashet(pipe myPipe, znachenia parametr, vector<double>& current_layer, vector<double>& previous_layer) {
+    vector<double> initial_layer(parametr.massiv);
+    // initial_layer - слой, значениями из которого проинициализируются все слои буфера
+
+           // Расчет методом характеристик
+        // Суть - смещение предыдущего слоя и запись граничного условия
+    for (size_t i = 1; i < current_layer.size(); i++)
+    {
+        current_layer[i] = previous_layer[i - 1];
+    }
+    current_layer[0] = parametr.nachalo;
+
+
+    // Слой current_layer на следующем шаге должен стать предыдущим. 
+    // Для этого сместим индекс текущего слоя в буфере на единицу
+
+}
+
+
+
+void excel(pipe myPipe, vector<double>& current_layer, int i) {
+    if (i == 0) {
+        ofstream outFile("ddddddddddddddddd.csv");
+        outFile << "время,координата,плотность, сера" << "\n";
+        // Записать значения текущего слоя в файл
+        for (size_t j = 0; j < current_layer.size(); j++) {
+            outFile << i * myPipe.dt << "," << j * myPipe.dx << "," << current_layer[j] << "\n";
+        }
+        outFile.close();
+    }
+
+    else {
+
+        ofstream outFile("ddddddddddddddddd.csv", ios::app);
+        // Записать значения текущего слоя в файл
+        for (size_t j = 0; j < current_layer.size(); j++) {
+            outFile << i * myPipe.dt << "," << j * myPipe.dx << "," << current_layer[j] << "\n";
+        }
+        outFile.close();
+    }
+
+
+
+
+
+}
+int main()
+{
+
+
+
+    pipe myPipe;
+    myPipe.L = 100000;
+    myPipe.V = 1.5;
+
+    double ro_n = 850;
+    double ro_k = 860;
+    myPipe.dx = myPipe.L / (grid_size - 1);
+    myPipe.dt = myPipe.dx / myPipe.V; //условие Куранта
+    myPipe.N = (T / myPipe.dt);
+
+    znachenia ro;
+    ro.massiv = vector<double>(myPipe.N);
+    ro.massiv = { 850 }; //значения плотности на входе трубы
+    ro.nachalo = 950;
+
+    znachenia sera;
+    sera.massiv = vector<double>(myPipe.N);
+    sera.massiv = { 0.01 }; //значения серы на входе трубы
+    vector<double > odin(grid_size);
+    ring_buffer_t<vector<double>> buffer(2, odin);
+
+    for (size_t i = 0; i < myPipe.N; i++) {
+        rashet(myPipe, ro, buffer.current(), buffer.previous());
+        buffer.advance(1);
+        excel(myPipe, buffer.current(), i);
+
+    }
+
+
+
+
+}
