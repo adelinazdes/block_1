682cb2fd2434cd0870dd8fb852f94fad23e331df
adelinazdes <adelina.ch15@mail.com>
2024-01-02 00:17:11 +0300

    добавила комменты цикл записи в файл зависит от размера массива нач.значений, а не от заданного кол-во слоев

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,10 +12,11 @@
 
 using namespace std;
 
-struct znachenia
+/// структура исходных значений 
+struct znachenia 
 {
     vector<double> massiv;
-    double nachalo;
+    
 };
 
 struct pipe
@@ -27,6 +28,8 @@ int grid_size = 101;//размер сетки
 int T = 3000; //Заданный период времени 
 
 
+
+/// функция смещение слоев
 
 void rashet(pipe myPipe, double parametr, vector<double>& current_layer, vector<double>& previous_layer) {
 
@@ -40,18 +43,21 @@ void rashet(pipe myPipe, double parametr, vector<double>& current_layer, vector<
 }
 
 
+//фунция записи в файл эксель
 
 void excel(pipe myPipe, ring_buffer_t<vector<vector<double>>>& buffer, int i) {
     vector<vector<double>>& current_layer = buffer.current();
+    //1 слой с записью заголовка
     if (i == 0) {
         ofstream outFile("plotnostsera.csv");
-        outFile << "время,координиата,плотность, сера" << "\n";
+        outFile << "время, координиата, плотность, сера" << "\n";
         // Записать значения текущего слоя в файл
         for (size_t j = 0; j < current_layer[0].size(); j++) {
             outFile << i * myPipe.dt << "," << j * myPipe.dx << "," << current_layer[0][j] << "," << current_layer[1][j] << "\n";
         }
         outFile.close();
     }
+    //последующие слои
     else {
         ofstream outFile("plotnostsera.csv", ios::app);
         // Записать значения текущего слоя в файл
@@ -63,8 +69,6 @@ void excel(pipe myPipe, ring_buffer_t<vector<vector<double>>>& buffer, int i) {
 }
 
 
-
-
 int main()
 {
     pipe myPipe;
@@ -72,25 +76,25 @@ int main()
     myPipe.V = 1;
 
 
-    myPipe.dx = myPipe.L / (grid_size - 1);
+    myPipe.dx = myPipe.L / (grid_size - 1);//расстояние между участками
     myPipe.dt = myPipe.dx / myPipe.V; //условие Куранта
-    myPipe.N = (T / myPipe.dt);
+    myPipe.N = (T / myPipe.dt);// кол-во слоев
 
     znachenia ro;
-    ro.massiv = vector<double>(myPipe.N);
+    
     ro.massiv = { 850,860,890 }; //значения плотности на входе трубы
-    ro.nachalo = 950;
+    
 
     znachenia sera;
-    sera.massiv = vector<double>(myPipe.N);
+    
     sera.massiv = { 0.01, 0.02, 0.03 }; //значения серы на входе трубы
 
 
-    vector<double> odin(grid_size);
-    ring_buffer_t<vector<vector<double>>> buffer(2, { odin, odin });
+    vector<double> odin(grid_size);//вектор с размером = кол-ву точек сетки
+    ring_buffer_t<vector<vector<double>>> buffer(2, { odin, odin });//создание буфера с 2 слоями для 2 векторов 
 
-
-    for (size_t i = 0; i < myPipe.N; i++) {
+    //цикл записи в файл
+    for (size_t i = 0; i < ro.massiv.size(); i++) {
         rashet(myPipe, ro.massiv[i], buffer.current()[0], buffer.previous()[0]);
         rashet(myPipe, sera.massiv[i], buffer.current()[1], buffer.previous()[1]);
 
